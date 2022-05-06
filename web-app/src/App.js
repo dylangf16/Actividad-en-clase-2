fb051f5a9909196736b56f9929b501eb76894b91
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2022-05-05 18:32:35 -0600

    Cambios

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,17 +1,36 @@
-import { useState } from "react";
-import "./App.css";
-import { fetchHelloWorld } from "./services/HelloWorldService";
-function App() {
-  const [label, setLabel] = useState("");
-  const clickHandler = async () => {
+import React from 'react';
+import './App.css';
+import { fetchHelloWorld } from './services/HelloWorldService';
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTime: Date.now()
+    }
+    setInterval(() => {
+      this.setState({
+        currentTime: Date.now()
+      });
+    }, 1000)
+  }
+
+  async clickHandler(){
     const result = await fetchHelloWorld();
-    setLabel(result);
-  };
-  return (
-    <div className="App">
-      <button onClick={() => clickHandler()}>Call API</button>
-      {label}
-    </div>
-  );
+    this.setState({
+      label: result
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {new Date(this.state.currentTime).toLocaleString()}
+        <button onClick={() => this.clickHandler()}>Call API</button>
+        {this.state.label}
+      </div>
+    );
+  }
 }
+
 export default App;
